7a9d7a9e82e2babab3f1ac9113161d9954b0fdb3
Léo Garlenc-Ricci <leogricci.pro@gmail.com>
2026-02-01 16:29:28 -0600

    Print and variables practice

--- Chapters/Chapter_1/chapter1_basics.py
+++ Chapters/Chapter_1/chapter1_basics.py
@@ -1,1 +1,20 @@
-print("Hello nerds!")
+salary = 100000
+bonus_rate = 0.1
+
+total_salary = salary * (1 + bonus_rate)
+print(round(total_salary, 2))
+
+
+company_name = "DataWiz Inc."
+job_id = 101
+job_title = 'Data Anlalyst'
+job_salary = 125000
+job_wfh = True
+
+print("Job Description")
+print("===============")
+print("Company name: ", company_name)
+print("Job ID: ", job_id)
+print("Job Title: ", job_title)
+print("Job Salary: ", job_salary)
+print("Work From Home: ", job_wfh)
