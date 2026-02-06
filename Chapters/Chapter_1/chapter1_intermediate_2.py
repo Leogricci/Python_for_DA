95ea805d5a283a192ce3ec712db54526244dcfd1
Léo Garlenc-Ricci <leogricci.pro@gmail.com>
2026-02-06 17:25:49 -0600

    Loops

--- Chapters/Chapter_1/chapter1_intermediate_2.py
+++ Chapters/Chapter_1/chapter1_intermediate_2.py
@@ -71,3 +71,87 @@ print(leos_skills is core_skills)
 # >>: Right Shift
 
 
+## Loops 
+
+# Two types: for and wile
+# Syntax:
+
+# for loop that works like a foreach loop
+skills_list = ['sql', 'python', 'java', 'excel']
+
+for skill in skills_list:
+    if 'sql' in skill:
+        print('sql is in the skills')
+    else:
+        print('sql is not in the skills')
+
+
+# other example
+numbers = [0, 1, 2, 3, 4, 5]
+
+for number in numbers:
+    print(number)
+
+# Works with strings
+word = 'PYTHON'
+
+for char in word:
+    print(char)
+
+# Or dictionaries
+years_experience = {
+    'Luke': 3,
+    'Kelly': 6,
+    'Ken': 4,
+    'Alex': 3
+}
+
+for person in years_experience:
+    print(person) # this prints only the keys
+
+for years in years_experience.values():
+    print(years) # this prints only the values
+
+for person, years in years_experience.items():
+    print(person, years) # this prints both
+
+
+# Loop that checks if a person has more than 5 years experience
+for person, years in years_experience.items():
+    if(years >= 5):
+        print(f"{person} has {years} years of experience")
+
+
+
+# For an actual for loop (not foreach) use for i in range(n)
+# This iterates a certain number of times (n times) 
+# Start at 0 and increments i at each iteration until it reaches n-1 then stops
+
+# This for example is a loop that prints values from 0 to 9 (n-1)
+for i in range(10):
+    print(i)
+
+print('')
+
+# This prints values from 5 to 15
+for i in range(5, 16):
+    print(i)
+
+print(' ')
+
+# This prints odd numbers from 1 to 19
+for i in range(1, 20, 2):
+    print(i)
+
+
+## While loops
+
+# While loops check for a condition at each iteration (like a if condition in for loop)
+# the following will print the count and increment it by 1 for each iteration until it reaches 5  and stops
+print('')
+count = 1
+while count < 5:
+    print(count)
+    count += 1
+
+
