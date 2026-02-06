b61751df7914208fcd3dcd5b3af7169ba6209bb5
Léo Garlenc-Ricci <leogricci.pro@gmail.com>
2026-02-06 16:13:30 -0600

    Tuples and final reviews on data containers

--- Chapters/Chapter_1/chapter1_data_containers.py
+++ Chapters/Chapter_1/chapter1_data_containers.py
@@ -157,3 +157,52 @@ skills_set.add('sql')
 skills_set.remove('tableau')
 print(skills_set)
 
+# A good use of sets is to remove duplicates from a lists by converting it to a set
+duplicates_list = ['python', 'sql', 'tableau', 'python', 'statistics', 'sql', 'tableau']
+duplicates_set = set(duplicates_list)
+print(duplicates_set)
+
+
+## Tuples
+
+# Tuples are defined using ()
+# The particularity of tuples is that they are for fixed data. We can't add, remove, or append elements. They can't be altered
+
+leos_skills = ('python', 'sql', 'java', 'r')
+print(leos_skills)
+
+# It's elements can be accessed by indexing
+print(leos_skills[1])
+# or slicing
+print(leos_skills[0:2])
+
+# We can concatenate tuples 
+new_skills = ('excel', 'power bi')
+leos_new_skills = leos_skills + new_skills
+print(leos_new_skills)
+
+# It is possible to add elements to a tuple but this will create a new object rather than modifying the existing one
+skill_set_1 = ('python', 'java')
+print(id(skill_set_1))
+skill_set_2 = ('excel', 'sql', 'tableau')
+
+skill_set_1 += skill_set_2
+print(skill_set_1)
+
+# This doesn't really add elements to skill_set_1, it creates a new set with the same name.
+# this can be seen with id number of the sets (run id(skill_set_1) before and after the concatenation)
+print(id(skill_set_1))
+
+
+## Range
+
+# Just a quick review of range: range(start, end, step)
+# range(5) will give numbers from 0 (inclusive) to 5 (exclusive)
+print(range(5))
+
+# We can see that by converting it to a tuple
+print(tuple(range(5)))
+
+# We can give it other arguments like the starting point and the step range(start, finish, step)
+# The following gives us odds numbers between 1 and 50
+print(tuple(range(1, 51, 2)))
