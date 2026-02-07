30443c4c0548d3e00196ed559a6bfa235f7d27a6
Léo Garlenc-Ricci <leogricci.pro@gmail.com>
2026-02-06 22:35:54 -0600

    Exercise on list and dictionary

--- Chapters/Chapter_1/chapter1_exercise.py
+++ Chapters/Chapter_1/chapter1_exercise.py
@@ -1,1 +1,40 @@
 ## Practice Exercise
+
+# Filter what jobs to apply for based on a list of skills
+
+# List of skills
+my_skills = ['Python', 'SQL', 'Excel']
+
+# List of dictionaries containing different job roles and the skills necessary for each. 
+job_roles = [
+    {'role': 'Data Analyst', 'skills': ['Python', 'SQL', 'Excel']},
+    {'role': 'Data Scientist', 'skills': ['Python', 'R', 'Machine Learning', 'Deep Learning']},
+    {'role': 'Machine Learning Engineer', 'skills': ['Python', 'TensorFlow', 'PyTorch', 'Scikit-Learn']},
+    {'role': 'Data Engineer', 'skills': ['Python', 'Apache Spark', 'Hadoop', 'SQL']},
+    {'role': 'Business Intelligence Analyst', 'skills': ['Python', 'SQL', 'Tableau', 'Power BI', 'Excel']},
+    {'role': 'Quantitative Analyst', 'skills': ['R', 'Python', 'MATLAB', 'Statistics']},
+    {'role': 'Operations Analyst', 'skills': ['Python', 'SQL', 'Data Visualization', 'Process Improvement']},
+    {'role': 'Database Administrator', 'skills': ['SQL', 'Oracle', 'MySQL', 'Database Management']},
+    {'role': 'AI Engineer', 'skills': ['Python', 'TensorFlow', 'PyTorch', 'Computer Vision']},
+    {'role': 'Statistician', 'skills': ['R', 'SAS', 'Python', 'Statistical Modeling']}
+]
+
+job_applied = []
+
+for job in job_roles:
+    qualified = True
+
+    for skill in my_skills:
+        if skill not in job['skills']:
+            qualified = False
+            break
+    
+    if qualified == True:
+        job_applied.append(job['role'])
+
+print(job_applied)
+
+
+## Let's do the same with list comprehesion
+qualified_roles = [job['role'] for job in job_roles if all(skill in job['skills'] for skill in my_skills)]
+print(qualified_roles)
