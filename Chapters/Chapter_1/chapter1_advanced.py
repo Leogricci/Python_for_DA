9058fffb542faff6564112ad586a20141db4bc13
Léo Garlenc-Ricci <leogricci.pro@gmail.com>
2026-02-08 16:44:23 -0600

    user defined and built in functions

---
+++ Chapters/Chapter_1/chapter1_advanced.py
@@ -0,0 +1,150 @@
+## Functions
+
+# Functions are used to simplify and automate tasks 
+# We are going to see 5 different types of functions:
+    # Built in functions: print()
+    # User-defined functions: def my_function(): pass
+    # Lambda functions: lambda x: x + 1
+    # Standard library functions math.sqrt()
+    # third-party library functions: numpy.array()
+
+# Let's focus on the built in functions. Here are some examples
+print('herro')
+type('what??')
+len('length')
+range(0, 5)
+# We can look at the python documentation to see the list built in functions are availables
+
+# Here are some useful ones for data science
+data_salaries = [95000, 100000, 78000, 85000, 140000]
+
+# This finds the min/max
+print(min(data_salaries))
+print(max(data_salaries))
+
+# This sums the elemnts of the iterable object we give it (here an array)
+print(sum(data_salaries))
+
+# This sorts the array
+print(sorted(data_salaries))
+
+
+
+# Let's now look at user defined functions.
+base_salary = 100000
+bonus_rate = 0.1
+
+# Here is a function that calculates total salary from base_salary * (1 + bonus_rate)
+def calculate_salary(salary, bonus):
+    total_salary = round(salary * (1 + bonus), 2)
+    return total_salary
+
+new_salary = calculate_salary(base_salary, bonus_rate)
+print(new_salary)
+
+# We can declare arguments inside the function parenthesis
+def calculate_bonus(salary, bonus = 0.25):
+    total_salary = round(salary * (1 + bonus), 2)
+    return total_salary
+
+salary_bonus = calculate_bonus(base_salary)
+print(salary_bonus)
+
+# But if we pass the argument it will override the declaration
+salary_bonus_override = calculate_bonus(base_salary, bonus_rate)
+print(salary_bonus_override)
+
+
+# =============================================================================================
+
+
+# Lambda functions
+
+# They do the same job as use defined functions but are shorter and cooler
+# Syntax -> lambda x: x * 2
+
+# Example: multiply an argument by 2
+mul_two = lambda x: x * 2
+print(mul_two(50))
+
+# See its like lambda expression in C#.
+# You create a function inside a variablme and call it with the variable. It does the exact same job as user-defined functions but with less code.
+
+# Here is a function that calculates salary after a bonus
+
+bonus_salary = lambda salary, bonus: salary * (1 + bonus)
+print(bonus_salary(base_salary, bonus_rate))
+
+# Use lambda when possible as it shortens and simplifies the program and looks cooler
+
+# We can even shorten it more to fit it in one line by putting parenthesis and calling it after
+print((lambda x, y: x*2 + y)(2, 3))
+
+# With the samary example:
+print((lambda salary, bonus: salary * (1 + bonus))(base_salary, bonus_rate))
+
+
+
+
+# Okay let's look at a more particular example
+# Let's apply a bonus to all the salaries in a list
+salary_list = [100000, 90000, 75000, 80000, 97000, 140000]
+
+# First define a function for applying the bonus (fixed bonus)
+def calculate_bonus(salary, bonus = 0.1):
+    return round(salary * (1 + bonus), 2)
+
+# Now we can use it in list comprehension
+salary_bonus_list = [calculate_bonus(salary) for salary in salary_list]
+print(salary_bonus_list)
+
+
+# So that took 4 lines. Let's do it in 1
+print([(lambda salary, bonus = 0.1: round(salary * (1 + bonus)))(salary) for salary in salary_list])
+
+
+
+# Let's look at how to use lambda functions to clean up or filter data
+
+job_data = [
+    {
+        "job_title": "Data Scientist",
+        "job_skills": ["Python", "SQL", "Machine Learning", "Statistics"],
+        "remote": True
+    },
+    {
+        "job_title": "Data Analyst",
+        "job_skills": ["SQL", "Excel", "Tableau", "Python"],
+        "remote": False
+    },
+    {
+        "job_title": "Machine Learning Engineer",
+        "job_skills": ["Python", "PyTorch", "Scikit-learn", "Data Pipelines"],
+        "remote": True
+    },
+    {
+        "job_title": "Business Intelligence Analyst",
+        "job_skills": ["SQL", "Power BI", "Data Modeling", "Dashboards"],
+        "remote": False
+    },
+    {
+        "job_title": "Data Engineer",
+        "job_skills": ["Python", "SQL", "Apache Spark", "ETL Pipelines"],
+        "remote": True
+    }
+]
+
+# We want to find jobs that are remote and require python as a skill
+# We use the built-in function filter() with a lambda function: filter(lambda, iterable)
+# lits() converts it to a list
+
+print(list(filter(lambda job: job['remote'] == True and 'Python' in job['job_skills'], job_data)))
+
+
+# These are the two main ways to use lambda. In list comprehension and in built-in functions
+
+
+# =============================================================================================
+
+
+## Modules
